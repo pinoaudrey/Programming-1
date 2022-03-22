da2add85a99962abb9217f69ab9e7beefaa18107
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-03-22 17:22:52 -0500

    Add files via upload

--- Lab02/Exercise05/main.cpp
+++ Lab02/Exercise05/main.cpp
@@ -1,1 +1,34 @@
-2
+/* -----------------------------------------------------------------------------
+ * File Name:  main.cpp
+ * Executable: soda
+ * Name: Audrey Pino
+ * Assignment:   EECS-168 Lab02
+ * Description:  This program will take a number of sodas and then tell the user how many Fridge Cubes, Six-packs, and Single sodas it will be packaged as.
+ * Date: 090920
+ *
+ ---------------------------------------------------------------------------- */
+
+#include <iostream>
+#include <iomanip>
+#include<math.h>
+using namespace std;
+int main(){
+	int soda;
+	int fridgeCube;
+	int sixPack;
+	int single;
+	int r1;
+	int r2;
+	cout<< "How many sodas do you have? ";
+	cin >> soda;
+	cout<< fixed<< setprecision(1);
+	fridgeCube = soda / 24;
+	r1 = soda % 24;
+	sixPack = r1 / 6;
+	r2 = r1 % 6;
+	single = r2;
+	cout<< "Fridge Cubes: " << fridgeCube << endl;
+	cout<< "Six-packs: " << sixPack << endl;
+	cout<< "Singles: " << single << endl;
+	return(0);
+}
