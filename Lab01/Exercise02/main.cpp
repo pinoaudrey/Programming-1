826bd6c7d3dde3b99af599e2740e47aaee2ae6a0
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-03-22 17:20:53 -0500

    Add files via upload

--- Lab01/Exercise02/main.cpp
+++ Lab01/Exercise02/main.cpp
@@ -1,1 +1,11 @@
-2
+#include<iostream>
+int main(){
+	std::cout<< "My name is Audrey Pino.\n";
+	std::cout<< "I am still deciding if I want to major in Mechanical Engineering or EECS.\n";
+	std::cout<< "My hobbies are:.\n\t";
+	std::cout<< "Photography.\n\t";
+	std::cout<< "Drinking coffee.\n\t";
+	std::cout<< "traveling.\n";
+	std::cout<< "goodbye!.\n";
+	return(0);
+}
