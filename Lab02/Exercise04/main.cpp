f8587d08b2566bc2dcf8473a0f17dfdf59c987c1
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-03-22 17:23:11 -0500

    Add files via upload

--- Lab02/Exercise04/main.cpp
+++ Lab02/Exercise04/main.cpp
@@ -1,1 +1,20 @@
-2
+/* -----------------------------------------------------------------------------
+ * File Name:   main.cpp
+ * Executable: ASCII
+ * Name: Audrey Pino
+ * Assignment:   EECS-168 Lab02
+ * Description:  This program will cast a character to an int.
+ * Date: 090920
+ *
+ ---------------------------------------------------------------------------- */
+
+#include <iostream>
+using namespace std;
+	int main(){
+	char i;
+	cout<< "Input a character: ";
+	cin >> i;
+	cout<< "The ASCII value for " << "'i'" << " is: " << int(i) << endl;
+	cout<< "goodbye!" << endl;
+	return(0);
+	}
